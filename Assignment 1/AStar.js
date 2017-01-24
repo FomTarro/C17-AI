4df7731f67587920c91dba53df51237f4a501700
FomTarro <twfarro@wpi.edu>
2017-01-24 15:49:01 -0500

    Added support to differentiate walks and leaps

--- Assignment 1/AStar.js
+++ Assignment 1/AStar.js
@@ -19,8 +19,11 @@ parentNode;
 function Cell(x, y, complexity) {
     this.x = x;
     this.y = y;
+    // For each node, the cost of getting from the start node to that node.
     this.G = Number.MAX_VALUE;
+    // Heuristic value
     this.H = 0;
+     // For each node, the total cost of getting from the start node to the goal
     this.F = this.G + this.H;
     this.parentNode = undefined;
 
@@ -65,16 +68,18 @@ function AStarPath(start, goal){
 function Search(start, goal)
 {
     // Already evaluated nodes go here
-    var _closedSet = {};
+    var closedSet = {};
     // Currently discovered nodes that are also evaluated
-    var _openSet = {start};
+    var openSet = {start};
 
     start.G = 0;
 
-    while(_openSet.length > 0)
+    var facingDir
+
+    while(openSet.length > 0)
     {
         // sort list by lowest f-score
-        _openSet.sort(
+        openSet.sort(
             function (node1, node2) 
             {
                 return node1.F - node2.F;
@@ -82,30 +87,36 @@ function Search(start, goal)
         );
 
         // investigate lowest f-score first
-        var current = _openSet[0];
+        var current = openSet[0];
 
         if(current == goal)
         {
             // construct a path and exit this function
             return true;
         }
-        _openSet.remove(current);
-        _closedSet.add(current);
+        openSet.remove(current);
+        closedSet.add(current);
 
         var neighbors = GetAdjacentCoordinates(current, _map.width, _map.height);
         for(var i = 0; i < neighbors.length; i++){
             var neighborNode; // set this to neighbors[i].x, neighbors[i].y from the map structure
 
+            // TODO: factor in turning costs
+            if(neighbors[neighborNode] == "neighbor"){
+        
+            } // factor in the additional cost of leaping
+            else if(neighbors[neighborNode] == "leap"){
+                
+            }
             // check if node with these coordinates exists in the closed set
             // if so, continue
 
-            // assumes cost between each tile is just 1
-            // this would be where to insert heuristics
+            // update the G value
             var gTemp = current.G + 1;
 
-            if(!_openSet.contains(neighborNode)) // discover a new node
+            if(!openSet.contains(neighborNode)) // discover a new node
             {
-                _openSet.add(neighborNode);
+                openSet.add(neighborNode);
             }
             else if(gTemp >= neighborNode.G) // this is not a better path
             {
@@ -119,29 +130,40 @@ function Search(start, goal)
 
         return false;
     }
+}
 
 // Pass in a node and the extremes of the map, gets all valid adjacent coordinates. 
 // Assumes minimum bounds are zero. 
-// MIGHT NEED TO FILTER OUT DIAGONALS?
-function GetAdjacentCoordinates(node, maxWidth, maxHeight){
-    var coordList = {};
-    for(var x = -1; x <= 1; x++)
+function GetAdjacentCoordinates(node, map){
+    var maxWidth = map.width;
+    var maxHeight = map.height;
+    var mapList = {};
+    for(var x = -1; x <= 1; x+=2)
     {
-         for(var y = -1; y <= 1; y++)
+         for(var y = -1; y <= 1; y+=2)
          {
              var xCoord = (node.x + x);
+             var xLeapCoord = (node.x + (3*x));
              var yCoord = (node.y + y);
+             var yLeapCoord = (node.y + (3 *y));
+
+             var neighbor;
              // if the new coordinates are within map bounds, and not equal to the node itself
              if((xCoord <= maxWidth && xCoord >= 0) && (yCoord <= maxHeight && yCoord > 0) 
              && !(x == 0 && y == 0)){
-                 var coord = new Object();
-                 coord.x = xCoord;
-                 coord.y = yCoord;
-                 coordList.add(coord);
+                 neighbor = map[x][y];
+                 nodeList[coord] = "neighbor";
+             }
+             else if((xLeapCoord <= maxWidth && xLeapCoord >= 0) && (yLeapCoord <= maxHeight && yLeapCoord > 0) 
+             && !(x == 0 && y == 0)){
+                 neighbor = map[x][y];
+                 nodeList[coord] = "leap";
              }
          }
     }
-    return coordList;
+    return nodeList;
 }
 
+function EvaluateHeuristic(node, goal){
+    
 }
