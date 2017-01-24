188eceb6dacb68e33896de2743b790b90328de36
tiniminimini <migcornbread@gmail.com>
2017-01-24 16:11:38 -0500

    updated heuristics for dominate checks

--- Assignment 1/heuristics.js
+++ Assignment 1/heuristics.js
@@ -14,11 +14,16 @@ var h2;
 var h3;
 var h4;
 var h5;
-var heuristics = {h0, h1, h2, h3, h4, h5};
+
+// may not need these idk
 var curr_pos;
 var goal;
+var heuristics = {h0, h1, h2, h3, h4, h5};
 
-// dunno if we need these yet
+// total time spent on actions by the agent as of it's current position
+var total_time;
+
+// the absolute distances of the xy coordinates of the curr position an goal
 var vertical;
 var horizontal;
 
@@ -29,16 +34,50 @@ var y1;
 var x2;
 var y2;
 
-function setHeuristics(heuristics)
+function setHeuristics()
 {
 
-	// start with base heuristic of 0
-	h0 = 0;
+	//TODO: retrieve the xy coordinates of the positions of the agent and the goal here
 
-	// calculate the minimum heuristic based on abs distance from agent's current position and goal
+	// find the absolute distances of the x and y coordinates of the agent's current position and the goal
 	horizontal = findAbsDistanceOfX(x1, x2);
 	vertical = findAbsDistanceOfY(y1, y2);
-	h1 = Min(vertical, horizontal);
+
+	// start with base heuristic of 0
+	try {
+		h0 = 0;
+
+		// find the min heuristic
+		h1 = Min(vertical, horizontal);
+
+		// same for the max heuristic
+		h2 = Max(vertical, horizontal);
+
+		// find the "manhattan" heuristic
+		h3 = Manhattan(vertical, horizontal);
+
+		// find the true goal cost as of the current unit of time spent and store it as a heuristic
+		// (this is an admissable heuristic)
+		h4 = h3 / total_time;
+
+		// find a non-admissable heuristic
+		h5 = h4 * 3;
+	} catch(e)
+	{
+		alert("something went wrong assigning heuristics");
+	}
+
+
+	// TODO: check for dominate heuristics here
+	if (heuristics !== undefined) && (h0 <= h1 || h1 <= h2 || h2 <= h3 || h3 <= h4)
+	{
+		return heuristics;
+	}
+	else
+	{
+		throw new DominateException("One or multiple heuristics is invalid");
+	}
+
 
 }
 
@@ -61,7 +100,7 @@ function Min(vertical, horizontal)
 		return vertical;
 	}
 
-	if (horizontal > vertical)
+	if (vertical > horizontal)
 	{
 		return horizontal;
 	}
@@ -76,10 +115,29 @@ function Min(vertical, horizontal)
 
 function Max(vertical, horizontal)
 {
+	if (horizontal > vertical)
+	{
+		return horizontal;
+	}
 
+	if (horizontal < vertical)
+	{
+		return vertical;
+	}
+	if (horizontal == vertical)
+	{
+		return horizontal;
+	}
 }
 
 function Manhattan(vertical, horizontal)
 {
+	var manhattan_dist = vertical + horizontal;
+	return manhattan_dist;
+}
 
+function DominateException(message)
+{
+	this.message = message;
+	this.name = "DominateException";
 }
