61247a7214cd19cd2d33d9bf3b8083e64d310870
Alex Guerra <silverx@verizon.net>
2017-01-30 14:43:14 -0500

    Initialize Bins
    Randomly dump input into bins and change sample input to be very simple

--- Assignment 2/optimize.js
+++ Assignment 2/optimize.js
@@ -22,6 +22,13 @@ fs.readFile(_inputFile, 'utf-8', function (err, data){
 	charInput.forEach(function(d){
 		_input.push(parseInt(d));
 	});
+	
+	//stops the program if the input is not divisible by 9
+	if (_input.length % 9 !== 0) {
+    	console.error('Number of integers in input file is not divisible by 9');
+    	process.exit(1);
+	}
+	
 	console.log(_input);
 	Optimize();
 });
@@ -31,5 +38,33 @@ var _bin2 = [];
 var _bin3 = [];
 
 function Optimize(){
+	InitializeBins();
+	PrintBins();
+}
+
+//randomly assigns numbers to bins
+function InitializeBins(){
+	var input = _input;
+	var randomIndex;
+	var maxBinLength = _input.length / 3;
 	
+	while(input.length > 0){
+		randomIndex = Math.floor(Math.random() * input.length);
+		if(_bin1.length < maxBinLength){
+			_bin1.push(input.slice(randomIndex, randomIndex + 1));
+		}
+		else if(_bin2.length < maxBinLength){
+			_bin2.push(input.slice(randomIndex, randomIndex + 1));
+		}
+		else{
+			_bin3.push(input.slice(randomIndex, randomIndex + 1));
+		}
+		
+		input = input.slice(0, randomIndex).concat(input.slice(randomIndex + 1));
+	}
+}
+
+//print bins and their scores SCORE NOT IMPLEMENTED YET
+function PrintBins(){
+	console.log("Bin 1: " + _bin1 + "\nBin 2: " + _bin2 + "\nBin 3: " + _bin3);
 }
