9932a7bb69c7fc8ec8d52760a98259d88d73d8c3
Alex Guerra <silverx@verizon.net>
2017-01-30 13:21:02 -0500

    More framework done
    put a function to put the main code in and created the basic variables
    for the bins and input, also convered input file to array of ints

--- Assignment 2/optimize.js
+++ Assignment 2/optimize.js
@@ -12,3 +12,24 @@ var _optimizeType = process.argv[2];
 var _inputFile = process.argv[3];
 
 var _allowedTime = process.argv[4];
+
+var _input = [];
+
+//convert file to be a array of integers inserted into _input
+fs.readFile(_inputFile, 'utf-8', function (err, data){
+	if (err) throw err;
+	var charInput = data.split(" ");
+	charInput.forEach(function(d){
+		_input.push(parseInt(d));
+	});
+	console.log(_input);
+	Optimize();
+});
+
+var _bin1 = [];
+var _bin2 = [];
+var _bin3 = [];
+
+function Optimize(){
+	
+}
