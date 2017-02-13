10a7c33952b00391ed655195ef53bddc14331513
FomTarro <twfarro@wpi.edu>
2017-02-13 16:00:28 -0500

    sample.js accepts inputs

--- Assignment 3/sample.js
+++ Assignment 3/sample.js
@@ -2,3 +2,21 @@
 // import in network class
 
 //code to read in cli commands
+
+var fs = require('fs');
+
+if (process.argv.length < 5) {
+    console.error('Exactly two arguments required');
+    console.error('node sample.js <query node> <iterations> <observed node 1> <observed node 2> ...');
+    process.exit(1);
+}
+
+var _queryNode =  process.argv[2];
+var _iterations =  process.argv[3];
+var _observedNodes = [];
+
+// accept in all future args as nodes to observe
+for(var i = 4; i <  process.argv.length; i++){
+    var obs =  process.argv[i]
+    _observedNodes[i-4] = obs;
+}
