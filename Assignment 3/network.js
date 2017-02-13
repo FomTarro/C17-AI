07620afc2b4b4162da815314b0fa945ad25e81e2
bonbombs <kelly.zhang2306@gmail.com>
2017-02-13 15:55:08 -0500

    Commented network stuff

--- Assignment 3/network.js
+++ Assignment 3/network.js
@@ -1,6 +1,13 @@
 ///make the network here
 // feel free to rename shit if variables seem too vague
 
+/**
+ * Node Class
+ *  name        name of node
+ *  children    an array of children. if node is a leaf, then this is undefined
+ *  CPT         CPT function. Has different params depending on node.
+ *              example for Snow node: - node.CPT(true, CPT.Humidity.LOW, CPT.Temperature.MILD) 
+ */
 function Node(name, cpt, children) {
     this.name = name;
     this.children = children;
@@ -8,6 +15,16 @@ function Node(name, cpt, children) {
     return this;
 }
 
+/**
+ * Network Class
+ *  This class is the only class that gets exported to sample.js
+ *  Other classes must be referenced from Network in order to be seen by outside js files
+ * 
+ *  topNodes        nodes at the "top" of network. these nodes have no parents.
+ *                  if this is undefined, then check if Init has been called
+ *  Init            initializes the network. must be called to perform operations on graph
+ *  PrintNetwork    Prints network
+ */
 function Network() {
     this.topNodes;
 
@@ -49,101 +66,109 @@ function Network() {
     return this;
 }
 
-var network = new Network();
-network.Init();
-network.PrintNetwork();
-
-var Humidity = {
-    LOW: 0.2,
-    MEDIUM: 0.5,
-    HIGH: 0.3
-}
-
-var Temperature = {
-    WARM: 0.1,
-    MILD: 0.4,
-    COLD: 0.5
-}
-
-var Day = {
-    WEEKEND: 0.2,
-    WEEKDAY: 0.8
-}
-
+/**
+ * CPT Object
+ *  This is where we get probability numbers.
+ *  Functions are assigned to nodes in Init.  
+ * 
+ */
 var CPT = {
 
-    Humidity: Humidity,
+    // Humidity (LOW, MEDIUM, HIGH)
+    Humidity: {
+        LOW: 0.2,
+        MEDIUM: 0.5,
+        HIGH: 0.3
+    },
 
-    Temperature: Temperature,
+    // Temperature (WARM, MILD, COLD)
+    Temperature: {
+        WARM: 0.1,
+        MILD: 0.4,
+        COLD: 0.5
+    },
 
-    Day: Day,
+    // Day (WEEKDAY, WEEKEND)
+    Day: {
+        WEEKEND: 0.2,
+        WEEKDAY: 0.8
+    },
 
+    // Cloudy (true, false)
     Cloudy: function(isCloudy) {
         return isCloudy ? 0.9 : 0.3;
     },
 
+    // Icy (true, false)
+    //      depends on [Humidity, Temperature]
     Icy: function(isIcy, h, t) {
         if (isIcy) {
-            if (h == Humidity.LOW && t == Temperature.WARM) return 0.001;
-            if (h == Humidity.LOW && t == Temperature.MILD) return 0.01;
-            if (h == Humidity.LOW && t == Temperature.COLD) return 0.05;
-            if (h == Humidity.MEDIUM && t == Temperature.WARM) return 0.001;
-            if (h == Humidity.MEDIUM && t == Temperature.MILD) return 0.03;
-            if (h == Humidity.MEDIUM && t == Temperature.COLD) return 0.02;
-            if (h == Humidity.HIGH && t == Temperature.WARM) return 0.005;
-            if (h == Humidity.HIGH && t == Temperature.MILD) return 0.01;
-            if (h == Humidity.HIGH && t == Temperature.COLD) return 0.35;
+            if (h == CPT.Humidity.LOW && t == CPT.Temperature.WARM) return 0.001;
+            if (h == CPT.Humidity.LOW && t == CPT.Temperature.MILD) return 0.01;
+            if (h == CPT.Humidity.LOW && t == CPT.Temperature.COLD) return 0.05;
+            if (h == CPT.Humidity.MEDIUM && t == CPT.Temperature.WARM) return 0.001;
+            if (h == CPT.Humidity.MEDIUM && t == CPT.Temperature.MILD) return 0.03;
+            if (h == CPT.Humidity.MEDIUM && t == CPT.Temperature.COLD) return 0.02;
+            if (h == CPT.Humidity.HIGH && t == CPT.Temperature.WARM) return 0.005;
+            if (h == CPT.Humidity.HIGH && t == CPT.Temperature.MILD) return 0.01;
+            if (h == CPT.Humidity.HIGH && t == CPT.Temperature.COLD) return 0.35;
         } else {
-            if (h == Humidity.LOW && t == Temperature.WARM) return 1 - 0.001;
-            if (h == Humidity.LOW && t == Temperature.MILD) return 1 - 0.01;
-            if (h == Humidity.LOW && t == Temperature.COLD) return 1 - 0.05;
-            if (h == Humidity.MEDIUM && t == Temperature.WARM) return 1 - 0.001;
-            if (h == Humidity.MEDIUM && t == Temperature.MILD) return 1 - 0.03;
-            if (h == Humidity.MEDIUM && t == Temperature.COLD) return 1 - 0.02;
-            if (h == Humidity.HIGH && t == Temperature.WARM) return 1 - 0.005;
-            if (h == Humidity.HIGH && t == Temperature.MILD) return 1 - 0.01;
-            if (h == Humidity.HIGH && t == Temperature.COLD) return 1 - 0.35;
+            if (h == CPT.Humidity.LOW && t == CPT.Temperature.WARM) return 1 - 0.001;
+            if (h == CPT.Humidity.LOW && t == CPT.Temperature.MILD) return 1 - 0.01;
+            if (h == CPT.Humidity.LOW && t == CPT.Temperature.COLD) return 1 - 0.05;
+            if (h == CPT.Humidity.MEDIUM && t == CPT.Temperature.WARM) return 1 - 0.001;
+            if (h == CPT.Humidity.MEDIUM && t == CPT.Temperature.MILD) return 1 - 0.03;
+            if (h == CPT.Humidity.MEDIUM && t == CPT.Temperature.COLD) return 1 - 0.02;
+            if (h == CPT.Humidity.HIGH && t == CPT.Temperature.WARM) return 1 - 0.005;
+            if (h == CPT.Humidity.HIGH && t == CPT.Temperature.MILD) return 1 - 0.01;
+            if (h == CPT.Humidity.HIGH && t == CPT.Temperature.COLD) return 1 - 0.35;
         }
     },
 
+    // Snow (true, false)
+    //      depends on [Humidity, Temperature]
     Snow: function(isSnow, h, t) {
         if (isSnow) {
-            if (h == Humidity.LOW && t == Temperature.WARM) return 0.00001;
-            if (h == Humidity.LOW && t == Temperature.MILD) return 0.001;
-            if (h == Humidity.LOW && t == Temperature.COLD) return 0.01;
-            if (h == Humidity.MEDIUM && t == Temperature.WARM) return 0.00001;
-            if (h == Humidity.MEDIUM && t == Temperature.MILD) return 0.0001;
-            if (h == Humidity.MEDIUM && t == Temperature.COLD) return 0.25;
-            if (h == Humidity.HIGH && t == Temperature.WARM) return 0.0001;
-            if (h == Humidity.HIGH && t == Temperature.MILD) return 0.001;
-            if (h == Humidity.HIGH && t == Temperature.COLD) return 0.4;
+            if (h == CPT.Humidity.LOW && t == CPT.Temperature.WARM) return 0.00001;
+            if (h == CPT.Humidity.LOW && t == CPT.Temperature.MILD) return 0.001;
+            if (h == CPT.Humidity.LOW && t == CPT.Temperature.COLD) return 0.01;
+            if (h == CPT.Humidity.MEDIUM && t == CPT.Temperature.WARM) return 0.00001;
+            if (h == CPT.Humidity.MEDIUM && t == CPT.Temperature.MILD) return 0.0001;
+            if (h == CPT.Humidity.MEDIUM && t == CPT.Temperature.COLD) return 0.25;
+            if (h == CPT.Humidity.HIGH && t == CPT.Temperature.WARM) return 0.0001;
+            if (h == CPT.Humidity.HIGH && t == CPT.Temperature.MILD) return 0.001;
+            if (h == CPT.Humidity.HIGH && t == CPT.Temperature.COLD) return 0.4;
         } else {
-            if (h == Humidity.LOW && t == Temperature.WARM) return 1 - 0.00001;
-            if (h == Humidity.LOW && t == Temperature.MILD) return 1 - 0.001;
-            if (h == Humidity.LOW && t == Temperature.COLD) return 1 - 0.01;
-            if (h == Humidity.MEDIUM && t == Temperature.WARM) return 1 - 0.00001;
-            if (h == Humidity.MEDIUM && t == Temperature.MILD) return 1 - 0.0001;
-            if (h == Humidity.MEDIUM && t == Temperature.COLD) return 1 - 0.25;
-            if (h == Humidity.HIGH && t == Temperature.WARM) return 1 - 0.0001;
-            if (h == Humidity.HIGH && t == Temperature.MILD) return 1 - 0.001;
-            if (h == Humidity.HIGH && t == Temperature.COLD) return 1 - 0.4;
+            if (h == CPT.Humidity.LOW && t == CPT.Temperature.WARM) return 1 - 0.00001;
+            if (h == CPT.Humidity.LOW && t == CPT.Temperature.MILD) return 1 - 0.001;
+            if (h == CPT.Humidity.LOW && t == CPT.Temperature.COLD) return 1 - 0.01;
+            if (h == CPT.Humidity.MEDIUM && t == CPT.Temperature.WARM) return 1 - 0.00001;
+            if (h == CPT.Humidity.MEDIUM && t == CPT.Temperature.MILD) return 1 - 0.0001;
+            if (h == CPT.Humidity.MEDIUM && t == CPT.Temperature.COLD) return 1 - 0.25;
+            if (h == CPT.Humidity.HIGH && t == CPT.Temperature.WARM) return 1 - 0.0001;
+            if (h == CPT.Humidity.HIGH && t == CPT.Temperature.MILD) return 1 - 0.001;
+            if (h == CPT.Humidity.HIGH && t == CPT.Temperature.COLD) return 1 - 0.4;
         }
     },
 
+    // Exams (true, false)
+    //      depends on [Snow, Day]
     Exams: function(isTrue, isSnow, day) {
         if (isTrue) {
-            if (!isSnow && day == Day.WEEKEND) return 0.001;
-            if (!isSnow && day == Day.WEEKDAY) return 0.01;
-            if (isSnow && day == Day.WEEKEND) return 0.0001;
-            if (isSnow && day == Day.WEEKDAY) return 0.3;
+            if (!isSnow && day == CPT.Day.WEEKEND) return 0.001;
+            if (!isSnow && day == CPT.Day.WEEKDAY) return 0.01;
+            if (isSnow && day == CPT.Day.WEEKEND) return 0.0001;
+            if (isSnow && day == CPT.Day.WEEKDAY) return 0.3;
         } else {
-            if (!isSnow && day == Day.WEEKEND) return 1 - 0.001;
-            if (!isSnow && day == Day.WEEKDAY) return 1 - 0.01;
-            if (isSnow && day == Day.WEEKEND) return 1 - 0.0001;
-            if (isSnow && day == Day.WEEKDAY) return 1 - 0.3;
+            if (!isSnow && day == CPT.Day.WEEKEND) return 1 - 0.001;
+            if (!isSnow && day == CPT.Day.WEEKDAY) return 1 - 0.01;
+            if (isSnow && day == CPT.Day.WEEKEND) return 1 - 0.0001;
+            if (isSnow && day == CPT.Day.WEEKDAY) return 1 - 0.3;
         }
     },
 
+    // Stress (high/true, low/false)
+    //      depends on [Snow, Exams]
     Stress: function(isHigh, isSnow, isExams) {
         if (isHigh) {
             if(!isSnow && !isExams) return 0.01;
@@ -158,3 +183,5 @@ var CPT = {
         }
     }
 }
+
+module.exports = Network; 
--- Assignment 3/sample.js
+++ Assignment 3/sample.js
@@ -1,4 +1,4 @@
 // import in probability library maybe
 // import in network class
-
+var Network = require("./network");
 //code to read in cli commands
