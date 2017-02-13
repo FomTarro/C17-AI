5d339b76f48b72adfef6a6557c51d56a60e61f82
FomTarro <twfarro@wpi.edu>
2017-02-13 16:21:42 -0500

    parses 'node=value' as key value pair

--- Assignment 3/sample.js
+++ Assignment 3/sample.js
@@ -4,6 +4,8 @@
 //code to read in cli commands
 
 var fs = require('fs');
+var Network = require("./network");
+
 
 if (process.argv.length < 5) {
     console.error('Exactly two arguments required');
@@ -11,14 +13,29 @@ if (process.argv.length < 5) {
     process.exit(1);
 }
 
-var _queryNode =  process.argv[2];
+var _queryNode =  nodeParser(process.argv[2]);
 var _iterations =  process.argv[3];
 var _observedNodes = [];
 
 // accept in all future args as nodes to observe
 for(var i = 4; i <  process.argv.length; i++){
-    var obs =  process.argv[i]
+    var obs =  nodeParser(process.argv[i]);
     _observedNodes[i-4] = obs;
 }
 
-var Network = require("./network");
+// splits along '=' delimiter 
+// .node is the node to look at
+// .value is the supplied value
+function nodeParser(node){
+
+    var splitNode = node.split('=');
+
+    var returnNode = new Object();
+    returnNode.node = splitNode[0];
+    returnNode.value = splitNode[1];
+
+    return returnNode;
+}
+
+var test = nodeParser("aaa=bbb");
+console.log(test.value);
