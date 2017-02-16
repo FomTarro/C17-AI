bbb0f70bec0f845c997961c849daaf778d605101
tiniminimini <migcornbread@gmail.com>
2017-02-15 22:14:46 -0500

    test cases added

---
+++ Final/move.js
@@ -0,0 +1,15 @@
+class Move {
+	/* possible callback functions not included:
+	 * basePowerCallback
+	 * onHit
+	 * onTryHit
+	*/
+
+	constructor (num, accuracy, basePower, category, desc, shortDesc, id, 
+		name, pp, priority, flags, isZ, critRatio, secondary, target, type,
+		contestType, drain, zMovePower, isViable, zMoveEffect, volatileStatus,
+		effect, multiHit, boosts, zMoveBoost, stallingMove)
+	{
+
+	}
+}
